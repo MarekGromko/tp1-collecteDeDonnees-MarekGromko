84c2e787cf34551ee2d2767c061b3400755834ff
Marek Gromko <marek.gromko@hotmail.ca>
2025-11-07 18:28:33 -0500

    fix genre string length in models

--- src/v2/model/serie.model.ts
+++ src/v2/model/serie.model.ts
@@ -10,7 +10,7 @@ export namespace Serie {
     export const schema = new Schema<Serie>({
         "title":  { type: String, required: true, minLength: 1, maxLength: 200 },
         "genres": { 
-            type: [{ type: String, required: true, min: 1, max: 30}],
+            type: [{ type: String, required: true, minLength: 1, maxLength: 30}],
             required: true
         },
         "status": {
